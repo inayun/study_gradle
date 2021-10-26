4bf3f14188f0d6feea52f5fc500a85b03a664ce5
Administrator <jangcm@ableu.com>
2021-10-25 21:11:11 -0700

    Initial commit

---
+++ src/main/java/io/study/study_gradle/StudyGradleApplication.java
@@ -0,0 +1,13 @@
+package io.study.study_gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/study/study_gradle/StudyGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.study.study_gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
